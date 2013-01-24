76ac441b7a9105d74757b9e97366ac162afa1022
lmastel@my.wctc.edu <>
2013-01-23 21:00:17 -0600

    first experiment

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -12,6 +12,6 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello Students");
-        System.out.println("Good bye");
+        System.out.println("Good ");
     }
 }
