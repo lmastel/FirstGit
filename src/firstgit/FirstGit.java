c0302eecae9bb376fb5d49824f53727dd1f6ac6c
lmastel@my.wctc.edu <>
2013-01-23 20:44:46 -0600

    first commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,16 @@
+
+package firstgit;
+
+/**
+ *
+ * @author LPM
+ */
+public class FirstGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
